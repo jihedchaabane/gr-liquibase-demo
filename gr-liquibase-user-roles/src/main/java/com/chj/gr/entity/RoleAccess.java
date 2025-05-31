922624d54956c4cae0032a4537c96a5fb63180f4
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-31 11:50:44 +0200

    other registered clients insert

--- gr-liquibase-user-roles/src/main/java/com/chj/gr/entity/RoleAccess.java
+++ gr-liquibase-user-roles/src/main/java/com/chj/gr/entity/RoleAccess.java
@@ -14,7 +14,7 @@ public class RoleAccess {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String name;
 
 	public Long getId() {
